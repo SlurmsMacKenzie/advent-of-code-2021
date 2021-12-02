50d8a742ddf08fea51fe4e53a803a46f9bc8b1c1
Moritz Buhr <moritz.buhr@gmx.net>
2021-12-02 13:28:52 +0100

    Refactor Day 2 solution with lambdas

--- day1/day2.py
+++ day1/day2.py
@@ -1,11 +1,5 @@
 from functools import reduce
 
-
-def countAscendingListElems(lst: list):
-    return len(list(filter(lambda x: x > 0 ,[j - i for i, j in zip(lst[: -1], lst[1:])])))
-def sumKeyValuesZippedList(lst: list, key:str):
-    return sum([w2 for (w1,w2) in  lst if w1 == key])
-
 with open("../inputs/day2.txt") as file:
     dataAsStrings = [(line.strip()).split() for line in file]
     directions, numbers = zip(*dataAsStrings)
@@ -13,24 +7,18 @@ with open("../inputs/day2.txt") as file:
     valueList = list(zip(directions, list(map(int, numbers))))
 
 # Part 1 ===
-    downSum = sumKeyValuesZippedList(valueList, 'down')
-    forwardSum  = sumKeyValuesZippedList(valueList, 'forward')
-    upSum  = sumKeyValuesZippedList(valueList, 'up')
-    print('Part 1:', (downSum - upSum) * forwardSum)
+    depth,dist = reduce(
+        lambda status, x: (status[0] + x[1], status[1]) if x[0] == 'down' else ( (status[0] - x[1], status[1]) if x[0] == 'up' else (status[0], status[1] + x[1]) ),
+        valueList, (0,0))
+
+    print('Part 1:', depth * dist)
+
 
 # Part 2 ===
-    depth = aim = dist = 0
-    for direction, value in valueList:
-        if direction == 'down':
-            aim += value
-        elif direction == 'up':
-            aim -= value
-        elif direction == 'forward':
-            dist += value
-            depth += aim*value
+    depth,_,dist = reduce(
+        lambda status, x: (status[0], status[1] + x[1], status[2]) if x[0] == 'down' else
+            ((status[0], status[1] - x[1], status[2]) if x[0] == 'up' else (status[0] + status[1] * x[1], status[1], status[2] + x[1])),
+            valueList,
+            (0, 0, 0))
 
     print('Part 2:', depth*dist)
-
-
-
-
